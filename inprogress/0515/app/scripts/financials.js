269f5d15be52c73af5ed5f2474956e4111701691
larrybe <larrybedesign@gmail.com>
2016-04-12 23:19:46 -0400

    IN-PROGRESS 0515

---
+++ inprogress/0515/app/scripts/financials.js
@@ -0,0 +1,9 @@
+﻿(function () {
+    angular.module('Financials', [])
+    .directive('highestGrossing', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'highestgrossing.html'
+        }
+    })
+})
