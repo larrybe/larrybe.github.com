4df80140887005f941b86673eeb8faa1c5dbe610
Larry Brobbey <larrybedesign@gmail.com>
2017-05-30 13:34:25 -0700

    events folder fix

--- events/main.bundle.js
+++ events/main.bundle.js
@@ -30,7 +30,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var GetEventsService = (function () {
     function GetEventsService(http) {
         this.http = http;
-        this.EventsUrl = "./../assets/events.json";
+        this.EventsUrl = "./../events/assets/events.json";
     }
     GetEventsService.prototype.getEvents = function () {
         return this.http.get(this.EventsUrl)
@@ -691,12 +691,8 @@ var EventsListComponent = (function () {
 
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
 var environment = {
-    production: false
+    production: true
 };
 //# sourceMappingURL=/Users/brobbey/larry/webdev/events/src/environment.js.map
 
